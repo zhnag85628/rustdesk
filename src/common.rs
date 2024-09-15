5490a06ddfa9951f979fb6e4612d92bb368c6c7d
zhnag85628 <83576164+zhnag85628@users.noreply.github.com>
2024-09-15 10:45:16 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -900,7 +900,7 @@ pub fn get_api_server(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://rjwl.x3322.net:21114".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
